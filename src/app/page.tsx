066703d3d3fd17e2c78202da1f7d02e967db4ae7
3illBaby <thrillanon@outlook.com>
2025-05-10 00:28:29 +0100

    feat(page.tsx): consumed headers

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import Header from "@/shared/layout/header";
+
 export default function Home() {
   return (
-    <section>
-      <h1 className="font-grotesk motion-preset-blur-up text-4xl font-bold text-white">
-        Hello World
-      </h1>
+    <section className="section_wrapper">
+      <Header>
+        <h1>Crypto-Tracker</h1>
+      </Header>
     </section>
   );
 }
