96863e4583c417558da565161d80a15723c44277
3illBaby <thrillanon@outlook.com>
2025-05-10 22:12:38 +0100

    chore(index.ts): updated coingecko keys

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,6 +2,10 @@ export const EnvConfig = {
   env: {
     coingecko: {
       API_KEY: process.env.COINGECKO_API_KEY || "",
+      BASE_URL: process.env.BASE_URL || "https://api.coingecko.com/api/v3",
+      HEADERS: {
+        accept: "application/json",
+      },
     },
   },
 };
