3bbe127348d477e5e324ec69cea6fe0b8b18b699
3illBaby <thrillanon@outlook.com>
2025-05-10 21:00:54 +0100

    chore(config): created global config

---
+++ src/config/index.ts
@@ -0,0 +1,7 @@
+export const EnvConfig = {
+  env: {
+    coingecko: {
+      API_KEY: process.env.COINGECKO_API_KEY || "",
+    },
+  },
+};
---
+++ src/config/schemas/env.ts
@@ -0,0 +1,13 @@
+import { z } from "zod";
+import { EnvConfig } from "..";
+
+const envSchema = z.object({
+  coingecko: z.object({
+    API_KEY: z
+      .string()
+      .min(1, "API_KEY is required")
+      .max(30, "API_KEY must be at most 30 characters"),
+  }),
+});
+
+export const env = envSchema.parse(EnvConfig.env.coingecko.API_KEY);
