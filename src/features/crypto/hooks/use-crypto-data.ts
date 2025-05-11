ce267025328d3d1cfc1240f29a7c2730b9966ca9
3illBaby <thrillanon@outlook.com>
2025-05-11 16:40:08 +0100

    Enhance useCryptoData hook with schema validation

--- src/features/crypto/hooks/use-crypto-data.ts
+++ src/features/crypto/hooks/use-crypto-data.ts
@@ -1,13 +1,24 @@
 import { useQuery, UseQueryResult } from "@tanstack/react-query";
-import { CryptoDataList } from "../schemas/crypto-data.schema";
-import { fetchCryptoData } from "../api/fetch-crypto-data";
+import {
+  CryptoDataList,
+  CryptoDataListSchema,
+} from "../schemas/crypto-data.schema";
+import { fetchCryptoData } from "../api/crypto-data/route";
 
 export const CRYPTO_DATA_QUERY_KEY = ["crypto", "data"];
 
 export const useCryptoData = (): UseQueryResult<CryptoDataList, Error> => {
   return useQuery({
     queryKey: CRYPTO_DATA_QUERY_KEY,
-    queryFn: fetchCryptoData,
+    queryFn: async () => {
+        try {
+          const data = await fetchCryptoData();
+          return CryptoDataListSchema.parse(data);
+        } catch (error) {
+          console.error("Error fetching crypto data:", error);
+          throw error;
+        }
+      },
     staleTime: 1000 * 30,
     refetchInterval: 1000 * 30,
     retry: 2,
