d5a26b62bfe39e13857d8441961f2ffb425616a7
3illBaby <thrillanon@outlook.com>
2025-05-11 16:37:03 +0100

    chore(index.ts): barrel export

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,8 +1,8 @@
 export const EnvConfig = {
   env: {
     coingecko: {
-      API_KEY: process.env.COINGECKO_API_KEY || "",
-      BASE_URL: process.env.BASE_URL || "https://api.coingecko.com/api/v3",
+      API_KEY: process.env.NEXT_PUBLIC_COINGECKO_API_KEY || "",
+      BASE_URL: process.env.NEXT_PUBLIC_BASE_URL || "https://api.coingecko.com/api/v3",
       HEADERS: {
         accept: "application/json",
       },
