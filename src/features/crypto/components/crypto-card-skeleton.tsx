a2489936c899e50fcd51fbba82a2b5886a7908a5
3illBaby <thrillanon@outlook.com>
2025-05-12 13:52:47 +0100

    refactor(crypto-card-skeleton.ts): updated skeleton

--- src/features/crypto/components/crypto-card-skeleton.tsx
+++ src/features/crypto/components/crypto-card-skeleton.tsx
@@ -2,28 +2,54 @@
 
 import React from "react";
 
-const CryptoCardSkeleton = () => {
-  return (
-    <div className="crypto_card_container">
-      <div className="flex flex-col p-4 h-full w-full gap-4">
-        {/* Crypto image skeleton */}
-        <div className="flex items-center gap-3">
-          <div className="h-10 w-10 rounded-full bg-neutral-600 animate-pulse" />
-          {/* Crypto name skeleton */}
-          <div className="h-5 w-24 rounded-md bg-neutral-600 animate-pulse" />
+const SkeletonItem = () => (
+  <div key={Math.random()}>
+    <div className="crypto_card_container px-2 py-4">
+      {/* 24hr Change */}
+      <div className="flex w-full flex-col">
+        <div className="h-5 w-16 rounded-md bg-neutral-600 animate-pulse self-end" />
+      </div>
+
+      {/* Name & Price */}
+      <div className="flex flex-col gap-[50px]">
+        <div className="center-items mt-2 justify-between">
+          {/* Crypto image skeleton */}
+          <div className="h-[50px] w-[50px] rounded-full bg-neutral-600 animate-pulse" />
+
+          <div className="flex flex-col">
+            {/* Crypto name skeleton */}
+            <div className="h-6 w-32 rounded-md bg-neutral-600 animate-pulse mb-1" />
+            {/* Price skeleton */}
+            <div className="h-5 w-24 rounded-md bg-neutral-600 animate-pulse" />
+          </div>
         </div>
-        
-        {/* Crypto price skeleton */}
-        <div className="mt-2">
-          <div className="h-7 w-32 rounded-md bg-neutral-600 animate-pulse" />
-        </div>
-        
-        {/* Percentage change skeleton */}
-        <div className="mt-auto">
-          <div className="h-6 w-20 rounded-md bg-neutral-600 animate-pulse" />
+
+        {/* 24h highs and lows */}
+        <div className="center-items justify-between">
+          <div className="flex flex-col gap-1">
+            <div className="h-4 w-16 rounded-md bg-neutral-600 animate-pulse" />
+            <div className="h-4 w-16 rounded-md bg-neutral-600 animate-pulse" />
+          </div>
+
+          <div className="flex flex-col gap-1">
+            <div className="h-4 w-16 rounded-md bg-neutral-600 animate-pulse" />
+            <div className="h-4 w-16 rounded-md bg-neutral-600 animate-pulse" />
+          </div>
         </div>
       </div>
     </div>
+  </div>
+);
+
+const CryptoCardSkeleton = () => {
+  return (
+    <section className="">
+      <div className="mt-[50px] grid w-full grid-cols-1 gap-y-16 sm:grid-cols-2 sm:pl-12 md:grid-cols-3 lg:pl-[80px]">
+        {[...Array(6)].map((_, index) => (
+          <SkeletonItem key={index} />
+        ))}
+      </div>
+    </section>
   );
 };
 
