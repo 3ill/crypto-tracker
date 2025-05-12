1c5d3ef3233c33f848859fc208b4770608fec80a
3illBaby <thrillanon@outlook.com>
2025-05-12 17:42:45 +0100

    chore(constant.ts): updated constants

--- src/constants/constant.ts
+++ src/constants/constant.ts
@@ -1,3 +1,13 @@
 export const MARKET_PATH =
   "/coins/markets?vs_currency=usd&ids=&ids=bitcoin%2Cethereum%2Csolana%2Cpolygon%2Cdogecoin";
 export const SEARCH_PATH = "/search?query=";
+
+export const SORT_FIELD = {
+  NAME: "name",
+  RANK: "market_cap_rank",
+} as const;
+
+export const SORT_ORDER = {
+  ASC: "asc",
+  DESC: "desc",
+} as const;
